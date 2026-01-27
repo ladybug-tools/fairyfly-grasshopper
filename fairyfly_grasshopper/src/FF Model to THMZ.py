70c1aeec55c0432f583558f9a2fb6dfacb9fa7a9
Chris Mackey <chris@ladybug.tools>
2026-01-27 13:41:20 -0800

    feat(therm): Expose Meshing Control on a dedicated component

--- fairyfly_grasshopper/src/FF Model to THMZ.py
+++ fairyfly_grasshopper/src/FF Model to THMZ.py
@@ -15,6 +15,10 @@ simulated or opened in the THERM interface.
     Args:
         _model: A fairyfly model object possessing all geometry and therm
             simulation properties.
+        _meshing_: An optional MeshControl object that describes how the input model
+            geometry shall be meshed for finite element analysis. This can be
+            generated from the "FF Meshing Control" component. Default: uses
+            the Simmetrix mesher with a parameter of 20.
         _folder_: An optional folder on this computer, into which the THMZ file
             will be written.
         _write: Set to "True" to write the THMZ file from the fairyfly Model.
@@ -28,13 +32,12 @@ simulated or opened in the THERM interface.
 
     Returns:
         report: A detailed report of the THMZ translation and simulation.
-        thmz: The file path of the SQL result file that has been generated on this
-            computer. This will be None unless run_ is set to True.
+        thmz: The file path of the THMZ file that has been written from the model.
 """
 
 ghenv.Component.Name = 'FF Model to THMZ'
 ghenv.Component.NickName = 'ModelToTHMZ'
-ghenv.Component.Message = '1.9.1'
+ghenv.Component.Message = '1.9.2'
 ghenv.Component.Category = 'Fairyfly'
 ghenv.Component.SubCategory = '1 :: THERM'
 ghenv.Component.AdditionalHelpFromDocStrings = '1'
@@ -51,6 +54,7 @@ except ImportError as e:
 try:
     from fairyfly_therm.config import folders as therm_folders
     from fairyfly_therm.writer import model_to_thmz
+    from fairyfly_therm.simulation.parameter import SimulationParameter
     from fairyfly_therm.run import run_model
 except ImportError as e:
     raise ImportError('\nFailed to import fairyfly_energy:\n\t{}'.format(e))
@@ -69,11 +73,17 @@ if all_required_inputs(ghenv.Component) and _write:
     _folder_ = os.path.join(folders.default_simulation_folder, model_name) \
         if _folder_ is None else _folder_
 
+    # process the simulation parameters
+    sim_par = SimulationParameter()
+    if _meshing_ is not None:
+        sim_par.mesh = _meshing_
+
     # write the model to THMZ or run it through THERM
     if not run_:
         thmz = os.path.join(_folder_, '{}.thmz'.format(model_name))
-        model_to_thmz(_model, thmz)
+        model_to_thmz(_model, thmz, simulation_par=sim_par)
     else:
         therm_folders.check_therm_version()
         silent = True if run_ > 1 else False
-        thmz = run_model(_model, _folder_, silent)
+        thmz = run_model(_model, simulation_par=sim_par,
+                         directory=_folder_, silent=silent)
